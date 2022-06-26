be5363cbdd9fa2a16159fece787504341042d7de
rakacha <rakeshkgec@gmail.com>
2022-06-26 17:51:55 +0530

    adding the restore script

---
+++ backup-restore/gdrive-sync-job/gdrive-download.py
@@ -0,0 +1,72 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def authenticate():
+	gauth = GoogleAuth()
+	gauth.CommandLineAuth()
+	drive = GoogleDrive(gauth)
+	return drive
+
+def findFolderIdByTitle(drive, folderName, parent_dir):
+	fileID = None
+	fileList = drive.ListFile({'q': "'%s' in parents and mimeType='application/vnd.google-apps.folder'and trashed=false" %parent_dir}).GetList()
+	for file in fileList:
+		print('Looking folder title: %s, ID: %s' % (file['title'], file['id']))
+		if(file['title'] == folderName):
+			fileID = file['id']
+			print(fileID)
+			break;
+		# Get the folder ID that you want
+		fileID = findFolderIdByTitle(drive, folderName, file['id'])
+	print(fileID)
+	return fileID;
+		
+import os	
+import shutil
+
+def restoreFiles(drive, folder_id, file_prefix):
+	site_config_backup = file_prefix + "-site_config_backup.json"
+	private_file_backup = file_prefix + "-private-files.tar"
+	public_file_backup = file_prefix + "-files.tar"
+	database_backup=file_prefix + "-database.sql.gz"
+	
+	shutil.rmtree('temp', ignore_errors=True)
+	os.mkdir("temp")
+	file_list = drive.ListFile({'q': "\'" + folder_id + "\'" + " in parents and trashed=false"}).GetList()
+	for file in file_list:
+		if(file['title'] == site_config_backup):
+			print('Downloading title: %s, id: %s' % (file['title'], file['id']))
+			file.GetContentFile(file['title'])
+			shutil.move("./"+site_config_backup, "./temp/"+site_config_backup)
+		if(file['title'] == private_file_backup):
+			print('Downloading title: %s, id: %s' % (file['title'], file['id']))
+			file.GetContentFile(file['title'])
+			shutil.move("./"+private_file_backup, "./temp/"+private_file_backup)
+		if(file['title'] == public_file_backup):
+			print('Downloading title: %s, id: %s' % (file['title'], file['id']))
+			file.GetContentFile(file['title'])
+			shutil.move("./"+public_file_backup, "./temp/"+public_file_backup)
+		if(file['title'] == database_backup):
+			print('Downloading title: %s, id: %s' % (file['title'], file['id']))
+			file.GetContentFile(file['title'])
+			shutil.move("./"+database_backup, "./temp/"+database_backup)
+
+
+
+
+	
+def restore():
+	if os.environ.get('RESTORE_FILE_PREFIX') is None:
+		print('No RESTORE_FILE_PREFIX environment variable is set.')
+		return
+	
+	if os.environ.get('RESTORE_PATH') is None:
+		print('No RESTORE_PATH environment variable is set.')
+		return
+		
+	file_prefix = os.environ.get('RESTORE_FILE_PREFIX')
+	drive = authenticate()
+	folder_id = findFolderIdByTitle(drive, os.environ.get('RESTORE_PATH'), 'root')
+	restoreFiles(drive, folder_id, file_prefix)
+	
+restore()
---
+++ backup-restore/script/lifi.eprnext.restore.gdrive.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo Running the restore job
+
+while getopts s:p:r:d: flag
+do
+    case "${flag}" in
+        s) site_name=${OPTARG};;
+        p) project_name=${OPTARG};;
+		r) file_name_prefix=${OPTARG};;
+		d) db_pass=${OPTARG};;
+     esac
+done
+
+echo "Site Name:"  $site_name
+echo "Project Name" $project_name
+echo "File Name Prefix" $file_name_prefix
+
+
+
+if [ -z "$site_name" ]
+ then
+  echo Site Names cannot be empty. for ex, -s site
+  exit
+fi
+
+if [ -z "$project_name" ]
+ then
+  echo Project Name cannot be empty. For ex, -p project
+  exit
+fi
+
+if [ -z "$file_name_prefix" ]
+ then
+  echo Restoring File Name Prefix cannot be empty. For ex, -r prefix
+  exit
+fi
+
+if [ -z "$db_pass" ]
+ then
+  echo DB root password cannot be empty. For ex, -d pass
+  exit
+fi
+
+echo "Restoring site:" $site_name
+docker run --name lifi-restore-job1  -e RESTORE_FILE_PREFIX=$file_name_prefix -e RESTORE_PATH=$site_name.backup --network frappe_docker_default rakacha/lifi-restore-job:1.0.0
+
+rm -r temp_drive_bcup
+mkdir temp_drive_bcup
+
+echo "Copying files from :" $site_name
+
+container_backup_path=/home/frappe/frappe-bench/sites/$site_name/private/backups
+
+docker cp ./temp_drive_bcup/. $project_name-backend-1:$container_backup_path
+
+docker container rm lifi-restore-job1
+
+docker exec $project_name-backend-1 bash bench --site $site_name restore --db-root-password $db_pass --with-public-files $container_backup_path/$file_name_prefix-files.tar --with-private-files  $container_backup_path/$file_name_prefix-private-files.tar $container_backup_path/$file_name_prefix-database.sql.gz
