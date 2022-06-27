faf1554305461e5e84ed954599d5f9d4e83c58f1
rakacha <rakeshkgec@gmail.com>
2022-06-27 10:13:30 +0530

    given the partial dir name

--- backup-restore/script/lifi.eprnext.backup.gdrive.sh
+++ backup-restore/script/lifi.eprnext.backup.gdrive.sh
@@ -35,8 +35,8 @@ for site_name in $site_arr
   docker exec $project_name-backend-1 bench --site $site_name backup --with-files
   export BACKUP_SITE_NAME=$site_name
   export BACKUP_GDRIVE_PATH=$site_name.backup
-  envsubst '${BACKUP_SITE_NAME},${BACKUP_GDRIVE_PATH}' < /home/azureuser/LIFI/devops/ffg/backup-restore/docker-compose-backup-template.yml > /home/azureuser/LIFI/devops/ffg/backup-restore/docker-compose-backup.yml
-  docker compose -f /home/azureuser/LIFI/devops/ffg/backup-restore/docker-compose-backup.yml up
+  envsubst '${BACKUP_SITE_NAME},${BACKUP_GDRIVE_PATH}' < $WORKING_DIR/ffg/backup-restore/docker-compose-backup-template.yml > $WORKING_DIR/ffg/backup-restore/docker-compose-backup.yml
+  docker compose -f $WORKING_DIR/ffg/backup-restore/docker-compose-backup.yml up
   unset BACKUP_SITE_NAME
   unset BACKUP_GDRIVE_PATH
  done
