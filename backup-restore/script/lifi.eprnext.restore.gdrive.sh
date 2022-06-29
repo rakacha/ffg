c0914a39ec42e9f67b140e26b978ddd9c87ea543
rakacha <rakeshkgec@gmail.com>
2022-06-29 09:39:54 +0530

    add site configuration

--- backup-restore/script/lifi.eprnext.restore.gdrive.sh
+++ backup-restore/script/lifi.eprnext.restore.gdrive.sh
@@ -50,20 +50,23 @@ if [ -z "$db_pass" ]
   exit
 fi
 
-echo "Restoring site:" $site_name
+echo "Downloading backups from Gdrive for site:" $site_name
 docker run --name lifi-restore-job1  -e RESTORE_FILE_PREFIX=$file_name_prefix -e RESTORE_PATH=$file_loc --network frappe_docker_default rakacha/lifi-restore-job:1.0.0
 
 rm -r temp_drive_bcup
 mkdir temp_drive_bcup
 
-echo "Copying files from restore container"
+echo "Copying files from restore container - lifi-restore-job1"
 docker cp lifi-restore-job1:/app/temp/. temp_drive_bcup
 
 echo "Removing container"
 docker container rm lifi-restore-job1
 
-echo "Copying to erpnext worker container"
-container_backup_path=/home/frappe/frappe-bench/sites/$site_name/private/backups
+echo "Copying sql dump and files to erpnext worker container"
+container_backup_path=/home/frappe/frappe-bench/sites/$site_name
+docker cp ./temp_drive_bcup/. $project_name-backend-1:$container_backup_path/private/backups
+
+echo "Copying site config files to erpnext worker container"
 docker cp ./temp_drive_bcup/. $project_name-backend-1:$container_backup_path
 
 echo "Executing the bench restore command"
