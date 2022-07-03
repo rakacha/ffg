a2e0c79546d4be61d1f159f3a461ff8fe856bb2f
rakacha <rakeshkgec@gmail.com>
2022-07-03 10:04:55 +0530

    iupdating the scripts and compose file

--- backup-restore/script/lifi.eprnext.backup.gdrive.sh
+++ backup-restore/script/lifi.eprnext.backup.gdrive.sh
@@ -31,19 +31,25 @@ site_arr=$(echo $site_names | tr "," "\n")
 for site_name in $site_arr
 
  do
-  echo "Processing site:" $site_name
+  echo "Backup process started for site:" $site_name
+  echo "Step 1: Running backup command for site: " $site_name
   docker exec $project_name-backend-1 bench --site $site_name backup --with-files
   
-  echo "Backup run, now processing to upload in Google Drive"
+  echo "Step 2: Backup run successfully, now processing to upload in Google Drive"
+  
   export SYNC_MODE=UPLOAD
   export BACKUP_SITE_NAME=$site_name
   export BACKUP_GDRIVE_PATH=$site_name.backup
   export WORKING_DIR=/home/azureuser/LIFI/devops
+  
   envsubst '${BACKUP_SITE_NAME},${BACKUP_GDRIVE_PATH}' < ${WORKING_DIR}/ffg/backup-restore/docker-compose-backup-template.yml > ${WORKING_DIR}/ffg/backup-restore/docker-compose-backup.yml
+  
   docker compose --project-name $project_name -f ${WORKING_DIR}/ffg/backup-restore/docker-compose-backup.yml up
+  
   unset BACKUP_SITE_NAME
   unset BACKUP_GDRIVE_PATH
   
+  echo "Backup prcoess completed for site: " $site_name
  done
 
 
--- backup-restore/script/lifi.eprnext.restore.gdrive.sh
+++ backup-restore/script/lifi.eprnext.restore.gdrive.sh
@@ -2,14 +2,15 @@
 
 echo Running the restore job
 
-while getopts s:p:l:r:d: flag
+while getopts s:p:l:f:r:d: flag
 do
     case "${flag}" in
         s) site_name=${OPTARG};;
         p) project_name=${OPTARG};;
 		l) file_loc=${OPTARG};;
-		r) file_name_prefix=${OPTARG};;
-		d) db_pass=${OPTARG};;
+		f) file_name_prefix=${OPTARG};;
+		r) db_pass=${OPTARG};;
+		d) lifi_db_pass=${OPTARG};;
      esac
 done
 
@@ -40,42 +41,64 @@ fi
 
 if [ -z "$file_name_prefix" ]
  then
-  echo Restoring File Name Prefix cannot be empty. For ex, -r prefix
+  echo Restoring File Name Prefix cannot be empty. For ex, -f prefix
   exit
 fi
 
 if [ -z "$db_pass" ]
  then
-  echo DB root password cannot be empty. For ex, -d pass
+  echo DB root password cannot be empty. For ex, -r pass
   exit
 fi
 
-echo "Downloading backups from Gdrive for site:" $site_name "from location: " $file_loc
+echo "Restore process started with backups from Gdrive for site:" $site_name "
+echo "Step 1: Downloading backups from Gdrive for site:" $site_name "from location: " $file_loc
+
 docker run -i -v gdrive-job-vol:/app --name gdrive-sync-job1 -e SYNC_MODE=DOWNLOAD -e RESTORE_FILE_PREFIX=$file_name_prefix -e RESTORE_PATH=$file_loc --network frappe_docker_default rakacha/lifi-grdrive-sync:1.0.0
+
+echo "Step 2: Copying files from gdrive sync job container: gdrive-sync-job1"
 
 rm -r temp_drive_bcup
 mkdir temp_drive_bcup
 
-echo "Copying files from restore container - gdrive-sync-job1"
 docker cp gdrive-sync-job1:/app/temp/. temp_drive_bcup
 
-echo "Removing container"
+if [ -z "$(ls -A temp_drive_bcup)" ]
+ then
+    echo "Downloaded backups successfully."
+ elif
+    echo "There is no backup file with the given file name. Please check if you have entered right file name prefix."
+	exit
+fi
+
+echo "Step 3: Removing gdrive sync job container : "
 docker container rm gdrive-sync-job1
 
-echo "Executing the create new site: " $site_name
-docker exec -i $project_name-backend-1 bash bench new-site $site_name --mariadb-root-password $db_pass --db-name $site_name.db --admin-password admin --install-app erpnext
+echo "Step 4: Executing the create new site: " $site_name
 
-echo "Enabling scheduler for site: " $site_name
+if [ -z "$lifi_db_pass" ]
+ then
+  echo "DB password provided  for application schema, creating the site with provided password."
+  docker exec -i $project_name-backend-1 bash bench new-site $site_name --mariadb-root-password $db_pass --db-name $site_name.db --db-password $lifi_db_pass --admin-password admin --install-app erpnext --force
+ elif
+  echo "DB password not provided  for application schema, creating the site with random password."
+  docker exec -i $project_name-backend-1 bash bench new-site $site_name --mariadb-root-password $db_pass --db-name $site_name.db --admin-password admin --install-app erpnext --force
+fi
+
+
+echo "Step 5: Enabling scheduler for site: " $site_name
 docker compose --project-name $project_name exec backend bench --site $site_name enable-scheduler
 
-echo "Copying sql dump and files to erpnext worker container"
+echo "Step 6: Copying sql dump and files to erpnext worker container"
 container_backup_path=/home/frappe/frappe-bench/sites/$site_name
 docker cp ./temp_drive_bcup/. $project_name-backend-1:$container_backup_path/private/backups
 
 
-echo "Executing the bench restore command for site: " $site_name
+echo "Step 7: Executing the bench restore command for site: " $site_name
 docker exec -i $project_name-backend-1 bash bench --site $site_name restore --db-root-password $db_pass --with-public-files $container_backup_path/private/backups/$file_name_prefix-files.tar --with-private-files  $container_backup_path/private/backups/$file_name_prefix-private-files.tar $container_backup_path/private/backups/$file_name_prefix-database.sql.gz
 
-#echo "Copying site config file: " $file_name_prefix-site_config_backup.json " to erpnext worker container"
-#docker cp ./temp_drive_bcup/$file_name_prefix-site_config_backup.json $project_name-backend-1:$container_backup_path/site_config.json
+if [ -z "$lifi_db_pass" ]
+ then
+	echo "Step 8: Copying site config file: " $file_name_prefix-site_config_backup.json " to erpnext site: " $site_name
+	docker cp ./temp_drive_bcup/$file_name_prefix-site_config_backup.json $project_name-backend-1:$container_backup_path/site_config.json
 
