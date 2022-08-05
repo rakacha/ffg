a5e7aa1e805466fe1294ef5660a9fd98bb9dad95
rakacha <rakeshkgec@gmail.com>
2022-08-05 14:22:07 +0530

    update custom app

---
+++ backup-restore/gdrive-sync-job_deprecated/gdrive_auth.py
@@ -0,0 +1,19 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def authenticate():
+	gauth = GoogleAuth()
+	gauth.CommandLineAuth()
+	drive = GoogleDrive(gauth)
+	return drive
+
+def listFiles(drive):
+	fileList = drive.ListFile({'q': "'root' in parents and mimeType='application/vnd.google-apps.folder'and trashed=false"}).GetList()
+	for file in fileList:
+		print('Looking folder title: %s, ID: %s' % (file['title'], file['id']))
+
+def testAuth():
+	drive = authenticate()
+	listFiles(drive)
+	
+
